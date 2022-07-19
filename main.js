89100516b26bf856570b92175cdd935eb2978f24
33YANG <yangyata@qq.com>
2022-07-19 15:53:56 +0800

    feat: add all file

---
+++ main.js
@@ -0,0 +1,129 @@
+const fs = require('fs')
+const fsp = require('fs').promises
+const axios = require('axios')
+const path = require('path')
+
+const BING_URL_PREFIX = 'https://cn.bing.com'
+const BING_WALLPAPER_CN_URL =
+  'https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=10&nc=1612409408851&pid=hp&FORM=BEHPTB&uhd=1&uhdwidth=3840&uhdheight=2160'
+const READ_ME_CONTENT = `
+# Bing Daily Wallpaper By NodeJS Script
+
+> This project is only for learning and communication, please do not spread or use it for illegal purposes
+
+## Last Week's Bing Wallpaper Preview
+`
+
+/**
+ * Main function, get daily bing wallpaper function
+ */
+async function fetchWallpaper() {
+  try {
+    const { data } = await axios.get(BING_WALLPAPER_CN_URL)
+    if (data.images.length) {
+      const lastWeekData = data.images
+      await writeReadMe(lastWeekData)
+
+      await writePreviewMarkdown(lastWeekData[0])
+
+      await downLoadWallpaper(lastWeekData[0])
+    }
+  } catch (error) {
+    console.log('Fetch Wallpaper Error Happened')
+    console.error(error)
+  }
+}
+
+/**
+ * write readme file
+ * @param {*} imageData wallpaper data
+ */
+const writeReadMe = async imageData => {
+  // rewrite readme.md
+  await fsp.writeFile(path.resolve(__dirname, './readme.md'), READ_ME_CONTENT, { flag: 'w' })
+
+  // append wallpaper image data
+  for (const item of imageData) {
+    const urlWithPrefix = `${BING_URL_PREFIX}${item.url}`
+
+    console.log(`[ ${new Date().toLocaleString()} ] [ Wallpaper Url ]`, urlWithPrefix)
+
+    await fsp.appendFile(path.resolve(__dirname, './readme.md'), '\n\r')
+    await fsp.appendFile(path.resolve(__dirname, './readme.md'), `- ${item?.enddate} -「 ${item?.title} 」 \n\r`)
+    await fsp.appendFile(path.resolve(__dirname, './readme.md'), `  ![${item?.title}](${urlWithPrefix})`)
+  }
+}
+
+/**
+ * save current date wallpaper to markdown file
+ * @param {*} imageData[0] current date wallpaper data
+ */
+const writePreviewMarkdown = async ({ url, title, enddate }) => {
+  const curDate = getCurDate()
+  const curMarkDownName = path.resolve(__dirname, `./preview/${curDate}.md`)
+
+  const isExist = fs.existsSync(curMarkDownName)
+  if (!isExist) {
+    await makeDirectory(path.resolve(__dirname, './preview'))
+    await fsp.writeFile(curMarkDownName, `# ${curDate} Wallpaper Preview \n\r`, { flag: 'w+' })
+  }
+
+  await fsp.appendFile(curMarkDownName, `- ${enddate} -「 ${title} 」\n\r`)
+  await fsp.appendFile(curMarkDownName, `  ![${title}](${BING_URL_PREFIX + url}) \n\r`)
+}
+
+/**
+ * download current date wallpaper
+ * @param {*} imageData[0] current date wallpaper data
+ */
+const downLoadWallpaper = async ({ url, title }) => {
+  const curMonthFolder = path.resolve(__dirname, `./wallpaper/${getCurDate()}`)
+  const isExist = fs.existsSync(curMonthFolder)
+  if (!isExist) {
+    await makeDirectory(curMonthFolder)
+  }
+  const { data: imageData } = await axios.get(BING_URL_PREFIX + url, {
+    responseType: 'arraybuffer',
+  })
+  const imageBuffer = Buffer.from(imageData, 'binary')
+
+  await fsp.writeFile(`${curMonthFolder}/${getCurDate(true)}.jpg`, imageBuffer)
+
+  console.log(`[ ${new Date().toLocaleString()} ] [ ${getCurDate(true)} ${title} Wallpaper Saved ]`)
+
+  // TODO: compress file
+  // if (new Date().getDate() === 1) {
+  //   const preMonthFolder = `./wallpaper/${new Date().getFullYear()}-${new Date().getMonth()}`
+  //   if (fs.existsSync(preMonthFolder)) {
+  //     const zip = new jsZip()
+  //     const content = await zip.folder(preMonthFolder).generateAsync({ type: 'nodebuffer' })
+  //     await fsp.writeFile(`./wallpaper/${new Date().getFullYear()}-${new Date().getMonth()}.zip`, content, { flag: 'w+' })
+  //     fs.rmdirSync(preMonthFolder, { recursive: true })
+  //   }
+  // }
+}
+
+/**
+ * make directory
+ * @param {*} path path
+ */
+const makeDirectory = async path => {
+  try {
+    await fsp.mkdir(path, { recursive: true })
+  } catch (error) {
+    console.log('Make Directory Error Happened')
+    console.error(error)
+  }
+}
+
+/**
+ * get current date and format
+ * @param {*} withDay is with day
+ * @returns current date like '2020-1-1'
+ */
+const getCurDate = withDay => {
+  const date = new Date()
+  return withDay ? `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}` : `${date.getFullYear()}-${date.getMonth() + 1}`
+}
+
+fetchWallpaper()
---
+++ readme.md
@@ -0,0 +1,23 @@
+
+# Bing Daily Wallpaper By NodeJS Script
+
+> This project is only for learning and communication, please do not spread or use it for illegal purposes
+
+## Last Week's Bing Wallpaper Preview
+
+- 20220719 -「 “巴伐利亚海”中的岛屿 」 
+  ![“巴伐利亚海”中的岛屿](https://cn.bing.com/th?id=OHR.FraueninselChiemsee_ZH-CN3541482552_UHD.jpg&rf=LaDigue_UHD.jpg&pid=hp&w=3840&h=2160&rs=1&c=4)
+- 20220718 -「 海上阿尔卑斯山 」 
+  ![海上阿尔卑斯山](https://cn.bing.com/th?id=OHR.OmijimaIsland_ZH-CN3328515301_UHD.jpg&rf=LaDigue_UHD.jpg&pid=hp&w=3840&h=2160&rs=1&c=4)
+- 20220717 -「 乘风破浪 」 
+  ![乘风破浪](https://cn.bing.com/th?id=OHR.CoyoteButtes_ZH-CN3166159419_UHD.jpg&rf=LaDigue_UHD.jpg&pid=hp&w=3840&h=2160&rs=1&c=4)
+- 20220716 -「 “薯片鸟” 」 
+  ![“薯片鸟”](https://cn.bing.com/th?id=OHR.AmericanGoldfinch_ZH-CN2996912015_UHD.jpg&rf=LaDigue_UHD.jpg&pid=hp&w=3840&h=2160&rs=1&c=4)
+- 20220715 -「 时光倒流...... 」 
+  ![时光倒流......](https://cn.bing.com/th?id=OHR.Arrone_ZH-CN2794175618_UHD.jpg&rf=LaDigue_UHD.jpg&pid=hp&w=3840&h=2160&rs=1&c=4)
+- 20220714 -「 自由自在的柠檬鲨 」 
+  ![自由自在的柠檬鲨](https://cn.bing.com/th?id=OHR.BabyLemons_ZH-CN4212701834_UHD.jpg&rf=LaDigue_UHD.jpg&pid=hp&w=3840&h=2160&rs=1&c=4)
+- 20220713 -「 巨石中的巨石 」 
+  ![巨石中的巨石](https://cn.bing.com/th?id=OHR.BasaltGiants_ZH-CN4038085235_UHD.jpg&rf=LaDigue_UHD.jpg&pid=hp&w=3840&h=2160&rs=1&c=4)
+- 20220712 -「 螺旋上升…… 」 
+  ![螺旋上升……](https://cn.bing.com/th?id=OHR.SpiralHill_ZH-CN3829252235_UHD.jpg&rf=LaDigue_UHD.jpg&pid=hp&w=3840&h=2160&rs=1&c=4)
